bd80966fa637234c59a4abb56620e26ba684e693
nanou-11 <anais.jouaret@gmail.com>
2020-06-18 10:57:04 +0200

    add test to route delete and put

--- test/users-posts.test.js
+++ test/users-posts.test.js
@@ -5,6 +5,19 @@ let server = require("../index");
 const sequelize = require("../sequelize");
 const Post = require("../models/Post");
 
+const errorKeys = ["status", "message"];
+const postKeys = [
+  "uuid",
+  "pseudo",
+  "score",
+  "TeamUuid",
+  "createdAt",
+  "updatedAt",
+  "Team",
+];
+let post;
+let id;
+
 chai.use(chaiHtpp);
 describe("POSTS", () => {
   before(async () => {
@@ -18,6 +31,18 @@ describe("POSTS", () => {
   });
   describe("Get all users posts", () => {
     it("should return an array of users posts", async () => {
+      try {
+        const res = await chai.request(server).get("/users/posts");
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.length.should.be.eql(1);
+      } catch (err) {
+        throw err;
+      }
+    });
+  });
+  describe("Get one user's post", () => {
+    it("should return an array of one user's posts", async () => {
       try {
         const res = await chai.request(server).get("/users/posts");
         res.should.have.status(200);
@@ -59,6 +84,28 @@ describe("POSTS", () => {
           .post("/users/posts")
           .send({ title: "Doe" });
         res.should.have.status(422);
+        res.body.should.be.a("object");
+      } catch (err) {
+        throw err;
+      }
+    });
+  });
+  describe("Put one user's post", () => {
+    it("should put one user's post", async () => {
+      try {
+        const res = await (await chai.request(server).put("/users/posts"))
+        res.should.have.status(202)
+        res.body.should.be.a("array");
+      } catch (err) {
+        throw err;
+      }
+    });
+  });
+  describe("Delete one user's post", () => {
+    it("should delete one user's post", async () => {
+      try {
+        const res = await (await chai.request(server).delete("/users/posts"))
+        res.should.have.status(205)
         res.body.should.be.a("object");
       } catch (err) {
         throw err;
