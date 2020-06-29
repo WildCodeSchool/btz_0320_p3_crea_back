a97dabca862de1d93c0e71825db1b4dd550fc665
Charlyln <charleshenry.lenue@gmail.com>
2020-06-29 10:10:28 +0200

    add partner model, partner route and http file

---
+++ models/Partner.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+
+const sequelizeInstance = require("../sequelize");
+
+const Partner = sequelizeInstance.define("Partner", {
+  id: {
+    type: Sequelize.UUID,
+    allowNull: false,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4,
+  },
+  label: {
+    type: Sequelize.STRING(200),
+    allowNull: false,
+  },
+  url: {
+    type: Sequelize.STRING(50),
+    allowNull: false,
+  },
+  logo: {
+    type: Sequelize.STRING(200),
+    allowNull: true,
+  },
+});
+
+module.exports = Partner;
---
+++ routes/partner.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const partners = express.Router();
+const Partner = require("../models/Partner");
+
+partners.get("/", async (req, res) => {
+  try {
+    const partner = await Partner.findAll();
+    res.status(200).json(partner);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+partners.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const partner = await Partner.findAll({ where: { id } });
+    res.status(200).json(partner);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+partners.post("/", async (req, res) => {
+  const { label, url, logo } = req.body;
+  try {
+    const partner = await Partner.create({
+      label,
+      url,
+      logo,
+    });
+    res.status(201).json(partner);
+  } catch (err) {
+    res.status(422).json(err);
+  }
+});
+
+partners.put("/:id", async (req, res) => {
+  const { label, url, logo } = req.body;
+  const { id } = req.params;
+  try {
+    const partner = await Partner.update(
+      {
+        label,
+        url,
+        logo,
+      },
+      { where: { id } }
+    );
+    res.status(202).json(partner);
+  } catch (err) {
+    res.status(422).json(err);
+  }
+});
+
+partners.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const partner = await Partner.destroy({ where: { id } });
+    res.status(205).send("Le partenaire d'utilisateur a bien été effacé");
+  } catch (err) {
+    res.status(422).json(err);
+  }
+});
+
+module.exports = partners;
