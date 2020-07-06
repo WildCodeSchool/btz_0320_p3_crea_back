ea74bb47aaf19b6fda214753a07156246c353f85
nanou-11 <anais.jouaret@gmail.com>
2020-07-06 10:50:03 +0200

    suppress alter true

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 if (process.env.NODE_ENV !== "test") {
   sequelize
-    .sync({ alter: true })
+    .sync()
     .then(() => {
       return sequelize.authenticate();
     })
