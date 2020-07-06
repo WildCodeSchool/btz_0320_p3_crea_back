52fd5b048a4d48c2bfc3f0ce39f734877123e450
nanou-11 <anais.jouaret@gmail.com>
2020-07-06 10:40:03 +0200

    add bcrypt to password

--- models/User.js
+++ models/User.js
@@ -1,49 +1,53 @@
 const Sequelize = require("sequelize");
 
+const bcrypt = require("bcrypt");
+
 const sequelizeInstance = require("../sequelize");
 
-const User = sequelizeInstance.define("User", {
+const User = sequelizeInstance.define(
+  "User",
+  {
     id: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        primaryKey: true,
-        defaultValue: Sequelize.UUIDV4,
+      type: Sequelize.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
     },
     lastName: {
-        type: Sequelize.STRING(100),
-        allowNull: false,
+      type: Sequelize.STRING(100),
+      allowNull: false,
     },
     firstName: {
-        type: Sequelize.STRING(100),
-        allowNull: false,
+      type: Sequelize.STRING(100),
+      allowNull: false,
     },
     email: {
-        type: Sequelize.STRING(100),
-        allowNull: false,
+      type: Sequelize.STRING(100),
+      allowNull: false,
     },
     password: {
-        type: Sequelize.STRING(14),
-        allowNull: false,
-        validate: {
-            max: 14,
-            min: 8,
-        },
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        max: 14,
+        min: 8,
+      },
     },
     localisation: {
-        type: Sequelize.STRING(50),
-        allowNull: false,
+      type: Sequelize.STRING(50),
+      allowNull: false,
     },
     country: {
-        type: Sequelize.STRING(50),
-        allowNull: false,
+      type: Sequelize.STRING(50),
+      allowNull: false,
     },
     phone_number: {
-        type: Sequelize.INTEGER(15),
-        allowNull: false,
+      type: Sequelize.INTEGER(15),
+      allowNull: false,
     },
     phone_number2: {
-        type: Sequelize.INTEGER(15),
-        allowNull: true,
+      type: Sequelize.INTEGER(15),
+      allowNull: true,
     },
     // isAdmin: {
     //     type: Sequelize.BOOLEAN,
@@ -51,46 +55,55 @@ const User = sequelizeInstance.define("User", {
     //     defaultValue: false,
     // },
     schoolName: {
-        type: Sequelize.STRING(50),
-        allowNull: true,
+      type: Sequelize.STRING(50),
+      allowNull: true,
     },
     companyName: {
-        type: Sequelize.STRING(50),
-        allowNull: true,
+      type: Sequelize.STRING(50),
+      allowNull: true,
     },
     siret: {
-        type: Sequelize.STRING(14),
-        allowNull: true,
+      type: Sequelize.STRING(14),
+      allowNull: true,
     },
     qualification: {
-        type: Sequelize.STRING(50),
-        allowNull: true,
+      type: Sequelize.STRING(50),
+      allowNull: true,
     },
     mobility: {
-        type: Sequelize.STRING(50),
-        allowNull: true,
+      type: Sequelize.STRING(50),
+      allowNull: true,
     },
     name_organisation: {
-        type: Sequelize.STRING(50),
-        allowNull: true,
+      type: Sequelize.STRING(50),
+      allowNull: true,
     },
     isActive: {
-        type: Sequelize.BOOLEAN,
-        allowNull: true,
+      type: Sequelize.BOOLEAN,
+      allowNull: true,
     },
     logo: {
-        type: Sequelize.STRING(200),
-        allowNull: true,
+      type: Sequelize.STRING(200),
+      allowNull: true,
     },
-});
+  },
+  {
+    hooks: {
+      beforeCreate: (user) => {
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(user.password, salt);
+      },
+    },
+  }
+);
 
 User.prototype.validPassword = function (password) {
- 
-    if (password === this.password) {
-        return true;
-    } else {
-        return false;
-    }
+  return bcrypt.compareSync(password, this.password);
+//   if (password === this.password) {
+//     return true;
+//   } else {
+//     return false;
+//   }
 };
 
 module.exports = User;
